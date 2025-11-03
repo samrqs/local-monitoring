097a2a29e5b1c776a0602d16b5515036e4db54c0
Samara Marques <samarassk@gmail.com>
2025-11-02 21:17:57 -0300

    update: reports > weather

--- backend/weather/apps.py
+++ backend/weather/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ReportsConfig(AppConfig):
+class WeatherConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reports'
+    name = 'weather'
